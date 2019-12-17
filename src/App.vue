5e16a60c7b473833373f1f87fba3dfe945025b41
gaochuanpei <364712763@qq.com>
2019-12-17 11:05:28 +0800

    finished

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,64 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <top />
+    <main-tv :items="items" @clickOn="goOn" />
+    <total-num :num="num" />
+    <div id="pb" v-show="state">加载中...</div>
+    <iframe v-show="state" src="" id="ck"></iframe>
   </div>
 </template>
-
+<script>
+import { getData } from "./network/home";
+import Top from "./components/Top";
+import MainTv from "./components/Main";
+import TotalNum from "./components/TotalNum";
+export default {
+  name: "App",
+  data() {
+    return {
+      items: null,
+      state: false,
+      num: 0
+    };
+  },
+  methods: {
+    goOn: function() {
+      this.state = true;
+    }
+  },
+  created() {
+    getData().then(res => {
+      this.num = res.shift()[0];
+      res.sort(function(a, b) {
+        return b[1] - a[1];
+      });
+      this.items = res;
+    });
+  },
+  components: {
+    Top,
+    MainTv,
+    TotalNum
+  }
+};
+</script>
 <style>
+#pb {
+  background-color: white;
+  text-align: center;
+}
+#ck,
+#pb {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
